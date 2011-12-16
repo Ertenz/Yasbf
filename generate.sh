0d59d26dbbe3342617f14fd8ca5757b7835bd82c
Poapfel <hello@poapfel.net>
2011-12-16 19:21:58 +0100

    Nothing big, just a few bug fixes and stuff...

--- generate.sh
+++ generate.sh
@@ -25,43 +25,28 @@ today=$(date)
 
 metafeed=$(awk '{sub(/\$name/,name);sub(/\$today/,today);sub(/\$description/,description);sub(/\$link/,link);}1' name="$name" today="$today" description="$description" link="$link" templates/feed.xml)
 
-meta=$(awk '{sub(/\$name/,name);sub(/\$author/,author);sub(/\$description/,description);}1' name="$name" author="$author" description="$description" templates/meta.html)
+header=$(awk '{sub(/\$name/,name);sub(/\$github/,github);sub(/\$description/,description);sub(/\$twitter/,twitter);sub(/\$author/,author);}1' name="$name" github="$github" description="$description" twitter="$twitter" author="$author" templates/header.html)
 
-header=$(awk '{sub(/\$name/,name);sub(/\$github/,github);sub(/\$description/,description);sub(/\$twitter/,twitter)}1' name="$name" github="$github" description="$description" twitter="$twitter" templates/header.html)
-
-cd templates
-if [ -f "article.html" ];
-then
-	rm article.html
-fi
-if [ -f "postfeed.xml" ];
-then
-	rm postfeed.xml
-fi
-cd ..
+footer=$(awk '{sub(/\$author/,author);sub(/\$year/,year);}1' author="$author" year="$year" templates/footer.html)
 
 cd posts
-for f in *
+for file in *
 do
-	aname=$(echo "$f" | sed 's/\(.*\)\..*/\1/')
-	headline="<h1><a href="\"\#$aname\"">$(sed -n 1p $f)</a></h1>"
-	postdate="<h3>$(sed -n 2p $f)</h3>"
-	article="$headline $postdate $(sed -n '4,$p' $f)"
-	postfeed="<item><title>$(sed -n 1p $f)</title><pubDate>$(sed -n 2p $f)</pubDate><description>$(sed -n '4,$p' $f)</description><link>$link/#$aname</link></item>"
+	postlink=$(echo "$file" | sed 's/\(.*\)\..*/\1/')
+	headline="<h1><a href="\"\#$postlink\"">$(sed -n 1p $file)</a></h1>"
+	postdate="<h3>$(sed -n 2p $file)</h3>"
+	article="$headline $postdate $(sed -n '4,$p' $file)"
+	postfeed="<item><title>$(sed -n 1p $file)</title><pubDate>$(sed -n 2p $file)</pubDate><description>$(sed -n '4,$p' $file)</description><link>$link/#$postlink</link></item>"
 	cd ..
-	cd templates
 	echo $article | tr '\r' ' ' >> article.html
 	echo $postfeed | tr '\r' ' ' >> postfeed.xml
-	cd ..
 	cd posts
 done
 cd ..
 
-article=$(cat templates/article.html)
+article=$(cat article.html)
 
-footer=$(awk '{sub(/\$author/,author);sub(/\$year/,year);}1' author="$author" year="$year" templates/footer.html)
-
-html="$meta $header <article>$article</article> $footer"
+html="$header <article>$article</article> $footer"
 
 if [ -f "index.html" ];
 then
@@ -75,7 +60,9 @@ then
 	rm feed.xml
 fi
 
-postfeed=$(cat templates/postfeed.xml)
+postfeed=$(cat postfeed.xml)
 feed="$metafeed $postfeed </channel></rss>"
 
 echo $feed | tr '\r' ' ' >> feed.xml
+rm article.html
+rm postfeed.xml
