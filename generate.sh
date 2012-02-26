b23b91789ee5d75af8ad8748452cddddde1a1900
Poapfel <hello@poapfel.net>
2012-02-26 21:20:43 +0100

    Added Flattr support (TO-DO Add Flattr to the Feed too)

--- generate.sh
+++ generate.sh
@@ -45,10 +45,13 @@ do
 	enclosure_url="$(sed -n 3p $filename)"
 	postcontent="$(sed -n '5,$p' $filename)"
 	postlink="$url/archives/$postdate/$filename"
+	if [ $flattr_id != "" ]; then
+		flattr="<a href=\"https://flattr.com/submit/auto?user_id=$flattr_id&url=$postlink&title=$postheadline&language=$flattr_lang&tags=$title&category=text\"><img src=\"http://api.flattr.com/button/flattr-badge-large.png\" /></a>"
+	fi
 	if [ "$(echo $(sed -n 3p $filename) | cut -c 1-4)" == "http" ]; then
-		article="<h1><a href=\"$postlink\">$postheadline</a></h1> <h3>$postdate</h3> $postcontent <audio controls=\"controls\"><source src="$enclosure_url" type="audio/mp3" /></audio>"
+		article="<h1><a href=\"$postlink\">$postheadline</a></h1> <h3>$postdate</h3> $postcontent <audio controls=\"controls\"><source src="$enclosure_url" type="audio/mp3" /></audio> $flattr"
 	else
-		article="<h1><a href=\"$postlink\">$postheadline</a></h1> <h3>$postdate</h3> $postcontent"
+		article="<h1><a href=\"$postlink\">$postheadline</a></h1> <h3>$postdate</h3> $postcontent $flattr"
 	fi
 	
 	#Generate the blog posts and the archive
